4dc73efd031a4117a0bb8001d8db8b2d22af4f85
Pete Woods <pete.woods@canonical.com>
2013-06-19 11:26:03 +0100

    Fix copyright headers

--- src/libusermetricsinput/MetricImpl.cpp
+++ src/libusermetricsinput/MetricImpl.cpp
@@ -1,8 +1,19 @@
 /*
- * MetricImpl.cpp
+ * Copyright (C) 2013 Canonical, Ltd.
  *
- *  Created on: 18 Jun 2013
- *      Author: pete
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of version 3 of the GNU Lesser General Public License as published
+ * by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Pete Woods <pete.woods@canonical.com>
  */
 
 #include <libusermetricsinput/MetricImpl.h>
