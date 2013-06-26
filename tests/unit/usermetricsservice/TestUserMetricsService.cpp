b15effcc300e2593e557c13fec2fd99383a10840
Pete Woods <pete.woods@canonical.com>
2013-06-26 12:50:13 +0100

    Lock down the tests a bit more

--- tests/unit/usermetricsservice/TestUserMetricsService.cpp
+++ tests/unit/usermetricsservice/TestUserMetricsService.cpp
@@ -18,10 +18,12 @@
 
 #include <usermetricsservice/DBusUserMetrics.h>
 #include <usermetricsservice/DBusDataSource.h>
+#include <usermetricsservice/DBusUserData.h>
 
 #include <testutils/QStringPrinter.h>
 
 #include <QDjango.h>
+
 #include <QSqlDatabase>
 
 #include <gtest/gtest.h>
@@ -37,41 +39,94 @@ static const QString bus = "unix:path=/tmp/usermetricsservice-test";
 
 class TestUserMetricsService: public Test {
 protected:
-	TestUserMetricsService() {
+	TestUserMetricsService() :
+			db(
+					QSqlDatabase::addDatabase("QSQLITE",
+							"test-user-metrics-service")), connection(
+					QDBusConnection::connectToBus(bus, "test-connection")) {
+		db.setDatabaseName(":memory:");
+		db.open();
+
+		QDjango::setDatabase(db);
 	}
 
 	virtual ~TestUserMetricsService() {
+		QSqlDatabase::removeDatabase("test-user-metrics-service");
 	}
+
+	QSqlDatabase db;
+
+	QDBusConnection connection;
 };
 
 TEST_F(TestUserMetricsService, PersistsDataSourcesBetweenRestart) {
-	QSqlDatabase db(QSqlDatabase::addDatabase("QSQLITE"));
-	db.setDatabaseName(":memory:");
-	db.open();
-
-	QDjango::setDatabase(db);
-
-	QDBusConnection connection(
-			QDBusConnection::connectToBus(bus, "test-connection"));
-
 	{
 		DBusUserMetrics userMetrics(connection);
+
+		EXPECT_TRUE(userMetrics.dataSources().empty());
 
 		EXPECT_EQ(QString("/com/canonical/UserMetrics/DataSource/facebook"),
 				userMetrics.createDataSource("facebook", "%1 messages received").path());
 
 		DBusDataSourcePtr facebook(userMetrics.dataSource("facebook"));
 		EXPECT_EQ(QString("facebook"), facebook->name());
+
+		{
+			QList<QDBusObjectPath> dataSources(userMetrics.dataSources());
+			EXPECT_EQ(1, dataSources.size());
+			EXPECT_EQ(QString("/com/canonical/UserMetrics/DataSource/facebook"),
+					dataSources.first().path());
+		}
 	}
 
 	{
 		DBusUserMetrics userMetrics(connection);
 
+		{
+			QList<QDBusObjectPath> dataSources(userMetrics.dataSources());
+			EXPECT_EQ(1, dataSources.size());
+			EXPECT_EQ(QString("/com/canonical/UserMetrics/DataSource/facebook"),
+					dataSources.first().path());
+		}
+
 		DBusDataSourcePtr facebook(userMetrics.dataSource("facebook"));
 		EXPECT_EQ(QString("facebook"), facebook->name());
 	}
+}
 
-	db.close();
+TEST_F(TestUserMetricsService, PersistsUserDataBetweenRestart) {
+	{
+		DBusUserMetrics userMetrics(connection);
+
+		EXPECT_TRUE(userMetrics.dataSources().empty());
+
+		EXPECT_EQ(QString("/com/canonical/UserMetrics/UserData/alice"),
+				userMetrics.createUserData("alice").path());
+
+		DBusUserDataPtr alice(userMetrics.userData("alice"));
+		EXPECT_EQ(QString("alice"), alice->username());
+
+		{
+			QList<QDBusObjectPath> userData(userMetrics.userData());
+			EXPECT_EQ(1, userData.size());
+			EXPECT_EQ(QString("/com/canonical/UserMetrics/UserData/alice"),
+					userData.first().path());
+		}
+	}
+
+	{
+		DBusUserMetrics userMetrics(connection);
+
+		{
+			QList<QDBusObjectPath> userData(userMetrics.userData());
+			EXPECT_EQ(1, userData.size());
+			EXPECT_EQ(QString("/com/canonical/UserMetrics/UserData/alice"),
+					userData.first().path());
+		}
+
+		DBusUserDataPtr alice(userMetrics.userData("alice"));
+		EXPECT_EQ(QString("alice"), alice->username());
+	}
 }
 
 } // namespace
