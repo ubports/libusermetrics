452c17000194989742ab0782ee4be42ddaacfc53
Ugo Riboni <ugo.riboni@canonical.com>
2013-09-17 13:03:34 +0200

    Add initial QML bindings with limited support for user metrics input only

---
+++ src/modules/UserMetrics/metric.cpp
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2013 Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "metric.h"
+
+#include <QDebug>
+#include <libusermetricsinput/MetricManager.h>
+
+Metric::Metric(QObject *parent) :
+    QObject(parent),
+    m_metric(0)
+{
+
+}
+
+QString Metric::name() const
+{
+    return m_name;
+}
+
+void Metric::setName(QString &name)
+{
+    if (name != m_name) {
+        m_name = name;
+        Q_EMIT nameChanged();
+        registerMetric();
+    }
+}
+
+QString Metric::format() const
+{
+    return m_format;
+}
+
+void Metric::setFormat(QString &format)
+{
+    if (format != m_format) {
+        m_format = format;
+        Q_EMIT formatChanged();
+        registerMetric();
+    }
+}
+
+void Metric::registerMetric()
+{
+    if (!m_name.isEmpty() && !m_format.isEmpty()) {
+        UserMetricsInput::MetricManager* manager;
+        manager = UserMetricsInput::MetricManager::getInstance();
+        if (manager) {
+            UserMetricsInput::MetricPtr metric = manager->add(m_name, m_format);
+            if (metric == 0) {
+                qWarning() << "Failed to register user metric:" << m_name << "\"" << m_format << "\"";
+            } else if (metric != m_metric) {
+                m_metric = metric;
+            }
+        }
+    }
+}
+
+void Metric::increment(float amount)
+{
+    if (m_metric) {
+        m_metric->increment(amount);
+    }
+}
+
+void Metric::update(float value)
+{
+    if (m_metric) {
+        m_metric->update(value);
+    }
+}
---
+++ src/modules/UserMetrics/metric.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2013 Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef METRICSMANAGER_H
+#define METRICSMANAGER_H
+
+#include <QObject>
+#include <libusermetricsinput/Metric.h>
+
+class Metric : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
+    Q_PROPERTY(QString format READ format WRITE setFormat NOTIFY formatChanged)
+
+public:
+    explicit Metric(QObject *parent = 0);
+    QString name() const;
+    void setName(QString& name);
+    QString format() const;
+    void setFormat(QString& format);
+
+public Q_SLOTS:
+    void increment(float amount = 1.0);
+    void update(float value);
+
+Q_SIGNALS:
+    void nameChanged();
+    void formatChanged();
+
+private:
+    void registerMetric();
+
+    QString m_name;
+    QString m_format;
+    UserMetricsInput::MetricPtr m_metric;
+};
+
+#endif // METRICSMANAGER_H
